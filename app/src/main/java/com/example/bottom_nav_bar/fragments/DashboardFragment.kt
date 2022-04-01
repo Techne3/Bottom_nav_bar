e223da1c715bc8936142bc309047e7838b272944
Timothy Sharkey <Techne3@gmail.com>
2022-04-01 12:19:35 -0600

    Setup bottom navigation

---
+++ app/src/main/java/com/example/bottom_nav_bar/fragments/DashboardFragment.kt
@@ -0,0 +1,22 @@
+package com.example.bottom_nav_bar.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.bottom_nav_bar.R
+
+
+class DashboardFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+}
